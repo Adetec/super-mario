7ea64dc7d7dd6b7d68b8645d3b6491dac8dfae39
Adel Lassag <adetech.aadl@gmail.com>
2019-06-26 22:02:01 +0100

    feat: Draw the sky background

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ import {loadImage} from './loaders.js';
 const canvas = document.getElementById('game-cv');
 const context = canvas.getContext('2d');
 
-context.fillRect(0, 0, 50, 50);
+// context.fillRect(0, 0, 50, 50);
 
 
 
@@ -13,6 +13,12 @@ loadImage('/images/tiles.png').then(image => {
     // Image source: https://www.spriters-resource.com/download/52571/
     const sprites = new SpriteSheet(image, 16,16);
     sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 32);
-    sprites.draw('sky', context, 45, 62); 
+    sprites.define('sky', 3, 23);
+    for (let x = 0; x < 25; x++) {
+        for (let y = 0; y < 14; y++) {
+            sprites.draw('sky', context, x * 16, y * 16);
+            
+        }
+        
+    }
 });
