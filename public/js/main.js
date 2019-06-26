7db52db34039894cad8a7612fdf4128894adaab6
Adel Lassag <adetech.aadl@gmail.com>
2019-06-26 22:22:10 +0100

    feat: Draw the ground

--- public/js/main.js
+++ public/js/main.js
@@ -14,11 +14,16 @@ loadImage('/images/tiles.png').then(image => {
     const sprites = new SpriteSheet(image, 16,16);
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
+    // Draw the sky
     for (let x = 0; x < 25; x++) {
         for (let y = 0; y < 14; y++) {
-            sprites.drawTile('sky', context, x, y);
-            
+            sprites.drawTile('sky', context, x, y);  
         }
-        
+    }
+    // Draw the ground
+    for (let x = 0; x < 25; x++) {
+        for (let y = 12; y < 14; y++) {
+            sprites.drawTile('ground', context, x, y);  
+        }
     }
 });
