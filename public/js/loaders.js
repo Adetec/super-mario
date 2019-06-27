821d3dfc403aa19abba244d46605003390a33444
Adel Lassag <adetech.aadl@gmail.com>
2019-06-27 01:29:54 +0100

    opt: Optimise json & image on loading

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -2,15 +2,22 @@ const loadImage = (url) => {
     return new Promise((resolve) => {
         const image = new Image();
         image.addEventListener('load', ()=> {
-            resolve(image);
-            console.log('Resolved');
+            setTimeout(() => {
+                resolve(image);
+                console.log('Image loaded',image);
+            }, 2000);
         });
         image.src = url;
     });
 }
 
 const loadLevel = (level) => {
-    return fetch(`./levels/${level}.json`).then(r => r.json());
+    return fetch(`./levels/${level}.json`).then(r => r.json()).then(json => {
+        return new Promise(resolve => setTimeout(() => {
+            resolve(json);
+            console.log('Json loaded', json);
+        }, 3000))
+    });
 }
 
 export {loadImage, loadLevel};
